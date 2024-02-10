1dc6545f529f2186eb6dfe9f858f47bfc967a681
GM347El <parkgm187@gmail.com>
2024-02-10 15:38:52 +0330

    add blazor and console

---
+++ CSharp/Pages/CSharpTutorial/Inheritance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp.Pages.CSharpTutorial
+{
+    public class InheritanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+//string dir = Directory.GetCurrentDirectory();
+string newDir=Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\Programming\Pages"));
+string[] dirs = Directory.GetFiles(newDir,"*.razor", SearchOption.AllDirectories);
+foreach (string directory in dirs)
+{
+    Console.WriteLine(directory.ToString());
+}
+//Console.WriteLine(dir);
+Console.WriteLine(newDir);
